3670b90d6789a51118b5cba734ff157f7370d855
Carlos Lopez <carlos.lopez@pragma.com.co>
2021-07-22 23:08:06 -0500

    Configs

---
+++ config-micro/src/main/java/com/pragma/config/ConfigMicroApplication.java
@@ -0,0 +1,15 @@
+package com.pragma.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMicroApplication.class, args);
+	}
+
+}
